afeb903a3782e489a348f80370f18bafc3c048f9
Mark Baydoun <markosbo95@gmail.com>
2020-06-13 16:22:01 +0200

    event controller

--- src/controller/event.ts
+++ src/controller/event.ts
@@ -38,3 +38,45 @@ const insertEventToDb = async (userId: string, name: string, description: string
     console.log(ex)
   }
 }
+
+const assignEventToGroup = async (userId: string, eventId: string, groupId: string) => {
+  try {
+    const group = await getConnection().getRepository(Group).findOne({ id: groupId })
+    const event = await getConnection().getRepository(Event).findOne({ id: eventId })
+    const user = await getConnection().getRepository(User).findOne({ id: userId })
+
+    const userGroupRelation = await getConnection()
+      .getRepository(UserGroup)
+      .findOne({ user: { id: userId }, group: { id: groupId } })
+    if (!userGroupRelation) throw new Error('User is not in group')
+    if (userGroupRelation.permissionLevel < 1) throw new Error('User does not have permission to perform this action')
+
+    await getConnection()
+      .createQueryBuilder()
+      .relation(Event, 'groups')
+      .of(event)
+      .add(group);
+
+    await getConnection()
+      .createQueryBuilder()
+      .relation(Group, 'events')
+      .of(group)
+      .add(event);
+
+    await getConnection()
+      .createQueryBuilder()
+      .relation(Event, 'users')
+      .of(event)
+      .add(user);
+
+    return { group, event }
+
+  } catch (ex) {
+    console.log(ex)
+  }
+}
+
+export {
+  insertEventToDb,
+  assignEventToGroup
+}

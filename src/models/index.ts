2ef2b5397ce68d70715cd61ede9d3ebf1b416cde
Mark Baydoun <markosbo95@gmail.com>
2020-06-14 14:31:37 +0200

    still working on controllers

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,9 +6,8 @@ import { Message } from './Group/Message'
 import { UserUser } from './User/UserUser'
 import { UserGroup } from './User/UserGroup'
 import { UserEvent } from './User/UserEvent'
-import { GroupEvent } from './Group/GroupEvent'
 import { Geolocation } from './Geolocation'
 // import { UserGeolocation } from './User/TODOUserGeolocation'
 // import { EventGeolocation } from './Event/TODOEventGeolocation'
 
-export { User, Group, Chat, Event, Message, UserUser, UserGroup, UserEvent, GroupEvent, Geolocation }
+export { User, Group, Chat, Event, Message, UserUser, UserGroup, UserEvent, Geolocation }
